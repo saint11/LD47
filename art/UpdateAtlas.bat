4ed1a2c8d917aeffcb499f2f2db1e5e4aef017d6
saint11 <saintjust@gmail.com>
2020-10-03 10:55:21 -0700

    Create UpdateAtlas.bat

---
+++ art/UpdateAtlas.bat
@@ -0,0 +1,6 @@
+ECHO off
+SET "PATH=D:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%"
+SET "PATH=C:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%"
+ECHO on
+TexturePacker hero.tps
+TexturePacker tiles.tps
