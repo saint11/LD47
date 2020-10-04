e217e63b9f19d01f65ca32ca1d53b6c938d5e80e
Amora B <amora.bettany@gmail.com>
2020-10-04 11:32:54 -0700

    Update UpdateAtlas.bat

--- art/UpdateAtlas.bat
+++ art/UpdateAtlas.bat
@@ -1,5 +1,5 @@
 ECHO off
-SET "PATH=D:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%"
+SET "PATH=D:/bin/;%PATH%"
 SET "PATH=C:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%"
 ECHO on
 TexturePacker hero.tps
